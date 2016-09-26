2aa9916f5c944b031b83906e7e16db2625b5295d
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-09-26 18:11:32 +1000

    Adding first exercise - variables.js

---
+++ variables.js
@@ -0,0 +1,12 @@
+var hello = 'Hello World!'; // Create a variable & set to Hello World
+
+hello = 'Hello Sheena!'; // take same variable & update it to Hello Sheena
+
+console.log(hello); // print it to screen
+console.log(hello); // print it to screen
+
+// Code alone exercise
+
+var name = 'Sheena';
+
+console.log(name);
