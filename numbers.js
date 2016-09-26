b34d5703395b83665baee8e3a310e36dad122247
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-09-26 18:57:50 +1000

    added numbers.js

---
+++ numbers.js
@@ -0,0 +1,41 @@
+// var age = 33;
+// var results;
+//
+// // addition
+// results = age + 6;
+// console.log('age + 6 = ' + results);
+//
+// // subtraction
+// results = age - 1;
+// console.log('age - 1 = ' + results);
+//
+// // multiplication
+// results = age * 3;
+// console.log('age x 3 = ' + results);
+//
+// // division
+// results = age / 6;
+// console.log('age / 6 = ' + results);
+//
+// // remainder - whatever's left over once you've finished dividing
+// results = age % 5;
+// console.log('age % 5 = ' + results);
+//
+// results++; //subtracts one from the result
+// console.log('minus 1: ' + results);
+//
+// var num = 19;
+// console.log(num++);
+// console.log(num);
+
+// self-coding exercise
+
+// 3 var each for a different test grade. From 0 - 100 %
+//add up the numbers, divide by the number of tests to get the average. Print out the person's average grade.
+
+var test1 = 100;
+var test2 = 80;
+var test3 = 90;
+var testAverage = (test1 + test2 + test3) / 3;
+
+console.log('Your test average is: ' +  testAverage + '%');
